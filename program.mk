7d9ee91d1ee3e52b3256beb0a426dbfc5681aa9a
FedericoMunoz <fmunozm@fi.uba.ar>
2020-11-05 02:09:25 -0300

    Modelos en Yakindu para TP1 (Panel,porton,app)

--- program.mk
+++ program.mk
@@ -1,2 +1,14 @@
+#Direccion en donde se encuentra nuestra carpeta de proyecto
+
+#default  : projects/yakindu-project
+#TP1      : projects
+
 PROGRAM_PATH = projects
-PROGRAM_NAME = yakindu-project
+
+#Nombre de la carpeta de proyecto
+
+#default                : blink
+#TP1 - Panel de control : panel_de_control
+#TP1 - Porton levadizo  : porton_levadizo
+
+PROGRAM_NAME = panel_de_control
